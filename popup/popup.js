2bcb888998815551fea903150bd2d8c21f9dae2a
BnjmnCummings <cummingsben097@gmail.com>
2024-11-09 14:47:47 +0000

    implemented tab grouping for two url queries

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,26 @@
-const tabs = await chrome.tabs.query({
+const googleTabs = await chrome.tabs.query({
   url: [
     'https://developer.chrome.com/docs/webstore/*',
     'https://developer.chrome.com/docs/extensions/*'
   ]
 });
 
+const jsTabs = await chrome.tabs.query({
+  url: [
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript*',
+    'https://www.w3schools.com/js*',
+    'https://developer.mozilla.org/en-US/docs/Learn/JavaScript*'
+  ]
+});
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
 const collator = new Intl.Collator();
-tabs.sort((a, b) => collator.compare(a.title, b.title));
+googleTabs.sort((a, b) => collator.compare(a.title, b.title));
+jsTabs.sort((a, b) => collator.compare(a.title, b.title));
 
 const template = document.getElementById('li_template');
 const elements = new Set();
-for (const tab of tabs) {
+for (const tab of googleTabs) {
   const element = template.content.firstElementChild.cloneNode(true);
 
   const title = tab.title.split('|')[0].trim();
@@ -29,11 +38,37 @@ for (const tab of tabs) {
 }
 document.getElementById('google-doc-list').append(...elements);
 
+elements.clear();
+for (const tab of jsTabs) {
+  const element = template.content.firstElementChild.cloneNode(true);
+
+  const title = tab.title.split('|')[0].trim();
+  const pathname = new URL(tab.url).pathname.slice('/docs'.length);
+
+  element.querySelector('.title').textContent = title;
+  element.querySelector('.pathname').textContent = pathname;
+  element.querySelector('a').addEventListener('click', async () => {
+    // need to focus window as well as the active tab
+    await chrome.tabs.update(tab.id, { active: true });
+    await chrome.windows.update(tab.windowId, { focused: true });
+  });
+
+  elements.add(element);
+}
+document.getElementById('js-doc-list').append(...elements);
+
 const button = document.querySelector('button');
 button.addEventListener('click', async () => {
-  const tabIds = tabs.map(({ id }) => id);
+  let tabIds = googleTabs.map(({ id }) => id);
   if (tabIds.length) {
+    console.log(tabIds);
     const group = await chrome.tabs.group({ tabIds });
     await chrome.tabGroups.update(group, { title: 'DOCS' });
+  }
+  tabIds = jsTabs.map(({ id }) => id);
+  if (tabIds.length) {
+    console.log(tabIds);
+    const group = await chrome.tabs.group({ tabIds });
+    await chrome.tabGroups.update(group, { title: 'JS Docs' });
   }
 });
