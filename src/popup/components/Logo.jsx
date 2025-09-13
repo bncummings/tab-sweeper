970ae78c8a28d4cdc1c7be08ff7461b147c278b8
BnjmnCummings <cummingsben097@gmail.com>
2025-09-13 12:31:18 +0100

    feat: added new logo

--- src/popup/components/Logo.jsx
+++ src/popup/components/Logo.jsx
@@ -8,13 +8,13 @@ const Logo = ({ width = 220, height = 80, color = '#333' }) => {
       gap: '6px'
     }}>
       <img 
-        src="images/logo2.svg" 
+        src="images/tabsweeper.svg" 
         alt="Tab Sweeper Logo" 
         style={{ 
           width: width + 'px', 
           height: height + 'px', 
           display: 'block',
-          filter: color === '#333' ? 'none' : 'brightness(0) invert(1)',
+          filter: 'brightness(0) invert(1)',
           maxWidth: '100%',
           objectFit: 'contain'
         }} 
